c63022aad7e5cfabb1d140b45f04ad509620a998
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2025-05-13 13:02:06 +0530

    Final cleanup: Remove temp_backend directory

--- server/temp_backend/server/routes/jobFilterRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { filterJobs } = require('../controllers/jobFilterController');
-
-// POST request to filter jobs
-router.post('/filter', filterJobs);
-
-module.exports = router;
